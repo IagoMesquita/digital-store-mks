b302f1c8b9fc6fd12f1d2fa101734e31036c916e
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-25 12:48:55 -0300

    feat: component Footer

---
+++ components/Footer/FooterStyled.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const FooterWrapper = styled.footer`
+  background: #EEEEEE;
+  width: 100vw;
+  height: 34px;
+`;
---
+++ components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FooterWrapper  } from './FooterStyled';
+function Footer() {
+  return (
+    <FooterWrapper>
+      <p>Footer</p>
+    </FooterWrapper>
+  )
+}
+
+export default Footer;
