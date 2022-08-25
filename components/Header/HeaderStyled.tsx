9e5074e2d5866686ff8bcc8e728f67d0b9c2c71f
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-25 11:27:39 -0300

    feat: component Header

---
+++ components/Header/HeaderStyled.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const HeaderWrapper = styled.header`
+  background: #0f52ba;
+  width: 375px;
+  height: 48px;
+`;
---
+++ components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { HeaderWrapper  } from './HeaderStyled';
+function Header() {
+  return (
+    <HeaderWrapper>
+      <p>Header</p>
+    </HeaderWrapper>
+  )
+}
+
+export default Header;
