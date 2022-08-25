32a93e0106474acc28d64f42a798a03b8c8bf4c3
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-25 15:58:11 -0300

    feat: CardProduc

---
+++ components/CardProduct/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { CardContainer } from './CardProductStyled';
+
+type Props = {}
+
+function index({}: Props) {
+  return (
+    <CardContainer>
+      Produto
+    </CardContainer>
+  )
+}
+
+export default index
