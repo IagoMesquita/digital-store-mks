a340818109f9b40b81c4563d80899c3ab82e68af
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-25 16:41:56 -0300

    chore: images in next.conf

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   optimizeFonts: true,
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['mks-sistemas.nyc3.digitaloceanspaces.com'],
+  },
 }
 
 module.exports = nextConfig
