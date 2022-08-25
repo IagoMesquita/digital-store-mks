c32a71a670155fb4ba40f51bdbc1b6886d2254e9
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-25 15:58:33 -0300

    style: CardProduct

---
+++ components/CardProduct/CardProductStyled.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const CardContainer = styled.div`
+  width: 250.5px;
+  height: 328px;
+  background: #FFFFFF;
+  box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.135216);
+  border-radius: 8px;
+
+  margin: 1.2em auto;
+`
