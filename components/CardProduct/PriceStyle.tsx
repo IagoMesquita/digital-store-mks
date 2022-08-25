c47b691172d705a1e7e5bd282b97a276bfc6f2b9
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-25 19:06:30 -0300

    style: price

---
+++ components/CardProduct/PriceStyle.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const PriceStyle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+  width: 73.69px;
+  height: 29.92px;
+  background:#373737;
+  border-radius: 5px;
+  
+  font-style: normal;
+  font-weight: 700;
+  font-size: 15px;
+  text-align: center;
+  color: #ffffff;
+`
