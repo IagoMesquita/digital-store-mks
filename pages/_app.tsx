ec3e4719bbfa614756370eb7f3336ec881eb7ae6
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-25 13:23:21 -0300

    feat: global style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app'
 import MainCotainer from '../components/MainCotainer'
+import GlobalStyled from '../styles/globalStyled'
 
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      <GlobalStyled />
       <MainCotainer>
         <Component {...pageProps} />
       </ MainCotainer>
