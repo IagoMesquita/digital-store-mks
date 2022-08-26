54b36777f03df55dbdd39d8d4c74466067b67c81
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-26 08:38:45 -0300

    refactor: clean files components e style cardproduc

--- components/CardProduct/CardProductStyled.tsx
+++ components/CardProduct/CardProductStyled.tsx
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 export const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
   width: 250.5px;
   height: 328px;
   background: #FFFFFF;
@@ -8,4 +12,64 @@ export const CardContainer = styled.div`
   border-radius: 8px;
 
   margin: 1.2em auto;
+  
+`
+
+export const InfoProductStyled = styled.div`
+  display: flex;
+  /* justify-content: space-between; */
+  padding: 0 .5em;
+  border: solid 1px red;
+  flex-wrap: wrap;
+
+  border: solid 1px blue;
+`
+
+export const PriceStyle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+  width: 73.69px;
+  height: 29.92px;
+  background:#373737;
+  border-radius: 5px;
+  
+  font-style: normal;
+  font-weight: 700;
+  font-size: 15px;
+  text-align: center;
+  color: #ffffff;
+`
+
+export const TextNames = styled.div`
+  /* display: flex;
+  justify-content: start; */
+
+  width: 124px;
+  height: 38px;
+
+  font-weight: 400;
+  font-size: 16px;
+  color: #2c2c2c;
+
+  border: solid 1px red;
+
+  p {
+    margin-right: .2em;
+    /* text-align: left; */
+
+  }
+`
+export const ButtonCard = styled.button`
+  background: #0F52BA;
+  border-radius: 0px 0px 8px 8px;
+  width: 100%;
+  height: 36.74px;
+  border: none;
+  
+  color: #ffffff;
+  text-transform: uppercase;
+  font-weight: 600;
+  font-size: 14px;
 `
--- components/CardProduct/index.tsx
+++ components/CardProduct/index.tsx
@@ -1,14 +1,43 @@
+import Image from 'next/image';
 import React from 'react';
-import { CardContainer } from './CardProductStyled';
+import formatCurrency from '../../utils/formartCurrency';
+import { IProduct } from '../../interface/IProduct';
+import { CardContainer, ButtonCard, PriceStyle, InfoProductStyled, TextNames } from './CardProductStyled';
+// import { ButtonCard } from './ButtonCardStyled';
+// import { PriceStyle } from './PriceStyled';
+// import { InfoProductStyled } from './InfoProductStyled';
+// import { TextNames } from './TextNamesStyled';
 
-type Props = {}
+interface ProductProp {
+  product: IProduct;
+}
 
-function index({}: Props) {
+function CardProduct({ product }: ProductProp) {
+
+  const addProduct = () => {
+    console.log(product.name)
+  }
+
   return (
     <CardContainer>
-      Produto
+      <div>
+        <Image style={ { margin: '0 auto' } } src={product.photo} alt={product.description} width={127.8} height={158.89}/>
+        <InfoProductStyled>
+          <TextNames>
+            <p>{`${product.brand} ${product.name}`}</p>
+          </TextNames>
+          <PriceStyle>
+            <span>R$</span>
+            { formatCurrency(Number(product.price)) }
+          </PriceStyle>
+        </InfoProductStyled>
+        <p>{product.description}</p>
+      </div>
+      <ButtonCard onClick={ addProduct } >
+        comprar
+      </ButtonCard>
     </CardContainer>
   )
 }
 
-export default index
+export default CardProduct;
