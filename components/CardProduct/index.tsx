dc9dae6ebb0b2a67b3c66b63b053dcf5e898f025
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-26 21:18:40 -0300

    refactor: clean code

--- components/CardProduct/index.tsx
+++ components/CardProduct/index.tsx
@@ -14,45 +14,20 @@ import {
 } from "./CardProductStyled";
 import { useDispatch, useSelector } from "react-redux";
 import { addProduct } from "../../redux/productSlice";
-// import { RootState } from "../../redux/store";
 
 interface ProductProp {
   product: IProduct;
 }
 
-type cart = string | null;
 
 function CardProduct({ product }: ProductProp) {
   const { id } = product;
   const [countProduct, setCountProduct] = useState({ un: 0 });
   const dispatch = useDispatch();
-  // const products = useSelector((state: RootState) => state.productCart)
-   
-  // const addProductLs = () => {
-  //   const cart = JSON.parse(localStorage.getItem('cart') || '') || [];
-  //   setCountProduct({ ...countProduct, un: countProduct.un + 1 });
-  //   const ifContained = cart.some((prod: ICart) => prod.id === id);
-  //   if (ifContained) {
-  //     cart.forEach((prod: ICart) => {
-  //       if (prod.id === id) {
-  //         prod.qtd += 1;
-  //       }
-  //     });
-  //     // localStorage.setItem('cart', JSON.stringify(cart));
-  //   } else {
-    // const cart: ICart[] = JSON.parse(localStorage.getItem('cart') || '') || [];
-  //     cart.push({ ...product, qtd: countProduct.un + 1 });
-  //     // localStorage.setItem('cart', JSON.stringify(cart));
-  //   }
-  //   localStorage.setItem('cart', JSON.stringify(cart));
-  // };
 
-
-  // const addProductInCart = (e: ChangeEvent<HTMLInputElement>) => {
-  const addProductInCart = () => {
-    dispatch(addProduct(product));
+  
+  const addProductLs = () => {
     const cart: ICart[] = JSON.parse(localStorage.getItem('cart')!) || [];
-    console.log('get', cart)
     setCountProduct({ ...countProduct, un: countProduct.un + 1 });
     const ifContained = cart.some((prod: ICart) => prod.id === id);
     if (ifContained) {
@@ -61,17 +36,16 @@ function CardProduct({ product }: ProductProp) {
           prod.qtd += 1;
         }
       });
-      // localStorage.setItem('cart', JSON.stringify(cart));
     } else {
       cart.push({ ...product, qtd: countProduct.un + 1 });
-      // localStorage.setItem('cart', JSON.stringify(cart));
     }
-    console.log('set', cart)
     localStorage.setItem('cart', JSON.stringify(cart));
-    // addProductLs()
-    // const { value } = e.target
-    // console.log('ddd', value)
+  };
 
+
+  const addProductInCart = () => {
+    dispatch(addProduct(product));
+    addProductLs()
   };
 
   return (
